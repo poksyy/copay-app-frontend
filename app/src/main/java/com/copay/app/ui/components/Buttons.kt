04fa24b586f01de609a13a377361f3757f744721
krschan <krischxn@gmail.com>
2025-03-06 18:09:55 +0100

    CHORE(Buttons) Add and remove unused imports.

--- app/src/main/java/com/copay/app/ui/components/Buttons.kt
+++ app/src/main/java/com/copay/app/ui/components/Buttons.kt
@@ -1,12 +1,11 @@
 package com.copay.app.ui.components
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.copay.app.R
 import com.copay.app.ui.theme.AppTypography
