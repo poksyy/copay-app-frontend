1ffd4c27bf7b1bdb72ab62188045ecf5c88eaf46
Dynavy <dynavy@hotmail.com>
2025-03-06 16:57:13 +0100

    FEAT(Buttons): Implemented Google Button componenent.

--- app/src/main/java/com/copay/app/ui/components/Buttons.kt
+++ app/src/main/java/com/copay/app/ui/components/Buttons.kt
@@ -6,15 +6,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.copay.app.R
 import com.copay.app.ui.theme.AppTypography
 import com.copay.app.ui.theme.Black
 import com.copay.app.ui.theme.LogoutButtonBackground
@@ -39,8 +43,8 @@ fun PrimaryButton(
     ) {
         Text(
             text = text,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Medium
+            color = White,
+            style = AppTypography.displayMedium
         )
     }
 }
@@ -63,8 +67,40 @@ fun SecondaryButton(
         Text(
             text = text,
             color = Black,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Medium
+            style = AppTypography.displayMedium
+        )
+    }
+}
+
+@Composable
+fun SignInWithGoogleButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(containerColor = SecondaryButtonFill),
+        border = BorderStroke(1.dp, SecondaryButtonBorder),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        shape = RoundedCornerShape(28.dp),
+    ) {
+        // Google icon 256x256.
+        Image(
+            painter = painterResource(id = R.drawable.google_256),
+            contentDescription = "Google Logo",
+            modifier = Modifier.size(24.dp)
+        )
+
+        // Space between the google image and the text.
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = text,
+            color = Black,
+            style = AppTypography.displayMedium
         )
     }
 }
@@ -90,38 +126,3 @@ fun LogoutButton(
         )
     }
 }
-
-//@Composable
-//fun SignInWithGoogleButton(
-//    text: String,
-//    onClick: () -> Unit,
-//    modifier: Modifier = Modifier
-//) {
-//    Button(
-//        onClick = onClick,
-//        colors = ButtonDefaults.buttonColors(containerColor = SecondaryButtonFill),
-//        border = BorderStroke(1.dp, SecondaryButtonBorder),
-//        modifier = modifier
-//            .fillMaxWidth()
-//            .height(56.dp),
-//        shape = RoundedCornerShape(28.dp),
-//    ) {
-//        // Logo de Google
-//        Image(
-//            painter = rememberImagePainter("file:///android_asset/google_logo.svg"), // Ruta del logo
-//            contentDescription = "Google Logo",
-//            modifier = Modifier.size(24.dp) // Ajusta el tamaño según sea necesario
-//        )
-//
-//        // Espaciado entre el logo y el texto
-//        Spacer(modifier = Modifier.width(8.dp))
-//
-//        // Texto del botón
-//        Text(
-//            text = text,
-//            fontSize = 16.sp,
-//            fontWeight = FontWeight.Medium,
-//            color = White
-//        )
-//    }
-//}
