73b0652ae00797cd8aa90e1198a2934b1392499e
krschan <krischxn@gmail.com>
2025-03-11 16:11:35 +0100

    FEAT(Auth): Implement UserRepository and API service for authentication.

--- app/src/main/java/com/copay/app/config/ApiService.kt
+++ app/src/main/java/com/copay/app/config/ApiService.kt
@@ -1,8 +1,11 @@
 package com.copay.app.config
 
-import retrofit2.http.GET
+import com.copay.app.model.User
 import okhttp3.ResponseBody
 import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
 
@@ -13,4 +16,12 @@ interface ApiService {
     // Method to test the connection with the client.
     @GET("${BASE_PATH}response")
     suspend fun getConnectionResponse(): Response<ResponseBody>
+
+    // Login API Call
+    @POST("${BASE_PATH}auth/login")
+    suspend fun loginUser(@Body request: User): Response<User>
+
+    // Register API Call
+    @POST("${BASE_PATH}auth/register")
+    suspend fun registerUser(@Body request: User): Response<User>
 }
