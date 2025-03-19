cfe4cfa1db33568d06c262f3c678855e820ba03c
Dynavy <dynavy@hotmail.com>
2025-03-19 18:02:59 +0100

    REFACTOR(AuthScreen): changed from HubScreen to AuthScreen
    - Changed the screen file name.
    
    - Changed all the references correctly.

--- app/src/main/java/com/copay/app/navigation/NavRoutes.kt
+++ app/src/main/java/com/copay/app/navigation/NavRoutes.kt
@@ -2,7 +2,7 @@ package com.copay.app.navigation
 
 sealed class NavRoutes(val route: String) {
 
-    data object HubScreen : NavRoutes("api/hub")
+    data object AuthScreen : NavRoutes("api/auth")
     data object RegisterScreen : NavRoutes("api/register")
     data object LoginScreen : NavRoutes("api/login")
     data object SplashScreen : NavRoutes("api/splash")
