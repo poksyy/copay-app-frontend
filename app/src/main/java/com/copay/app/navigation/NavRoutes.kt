feaa7f604bb2fad5b8577e6f742e4ff219fd3fd2
Dynavy <dynavy@hotmail.com>
2025-03-24 16:52:51 +0100

    FEAT(NavRouts): Added new navigation routes for HubScreen and its sections.
    - Added routes for Home, Plannings, Friends, and Profile screens.
    
    - Updated the HubScreen route to match the new navigation structure.

--- app/src/main/java/com/copay/app/navigation/NavRoutes.kt
+++ app/src/main/java/com/copay/app/navigation/NavRoutes.kt
@@ -2,8 +2,14 @@ package com.copay.app.navigation
 
 sealed class NavRoutes(val route: String) {
 
+    data object SplashScreen : NavRoutes("api/splash")
     data object AuthScreen : NavRoutes("api/auth")
     data object RegisterScreen : NavRoutes("api/register")
     data object LoginScreen : NavRoutes("api/login")
-    data object SplashScreen : NavRoutes("api/splash")
+
+    data object HubScreen : NavRoutes("api/hub")
+    data object HomeScreen : NavRoutes("api/home")
+    data object PlanningScreen : NavRoutes("api/planning")
+    data object FriendScreen : NavRoutes("api/friend")
+    data object ProfileScreen : NavRoutes("api/profile")
 }
