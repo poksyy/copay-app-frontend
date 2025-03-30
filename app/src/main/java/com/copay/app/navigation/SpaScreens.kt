1693637c2c61626082a7b5a98e44d81b58b0c4d7
Dynavy <dynavy@hotmail.com>
2025-03-30 23:49:58 +0200

    FEAT(ScreenRoutes): Added Join and Group routes to SPA navigation.
    - Added routes for Join and Group screens to manage group-related navigation.
    
    - Integrated new routes into the existing SPA navigation structure.

--- app/src/main/java/com/copay/app/navigation/SpaScreens.kt
+++ app/src/main/java/com/copay/app/navigation/SpaScreens.kt
@@ -2,8 +2,13 @@ package com.copay.app.navigation
 
 sealed class Screen(val route: String) {
 
+    // HomeScreen navigation routes.
     data object Home : Screen("home")
+    data object JoinGroup : Screen("join_group")
+    data object CreateGroup : Screen("create_group")
+
     data object Plannings : Screen("plannings")
     data object Friends : Screen("friends")
     data object Profile : Screen("profile")
+
 }
