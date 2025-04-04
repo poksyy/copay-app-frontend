0cb9e895f745f171bcb1eb0a3758df56e0ecc8eb
Dynavy <pl2023379@365.stucom.com>
2025-04-04 14:42:23 +0200

    REFACTOR(SpaScreens): Organized the code and added comments

--- app/src/main/java/com/copay/app/navigation/SpaScreens.kt
+++ app/src/main/java/com/copay/app/navigation/SpaScreens.kt
@@ -2,13 +2,14 @@ package com.copay.app.navigation
 
 sealed class SpaScreens(val route: String) {
 
-    // HomeScreen navigation routes.
+    // Main SpaScreens inside HubScreen.
     data object Home : SpaScreens("home")
-    data object JoinGroup : SpaScreens("join_group")
-    data object CreateGroup : SpaScreens("create_group")
-
     data object Plannings : SpaScreens("plannings")
     data object Friends : SpaScreens("friends")
     data object Profile : SpaScreens("profile")
+
+    // HomeScreen subpages inside the SPA.
+    data object JoinGroup : SpaScreens("join_group")
+    data object CreateGroup : SpaScreens("create_group")
 
 }
