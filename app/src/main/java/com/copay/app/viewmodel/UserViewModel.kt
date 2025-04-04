94b1d9a7a793f5cec1a135c8cd48a8df40ac498e
Dynavy <pl2023379@365.stucom.com>
2025-04-04 14:46:10 +0200

    FEAT(UserViewModel): Saves the extracted fields from the Backend.
    - The UserService layer saves the extracted data trough the new implemented setUser() method.

--- app/src/main/java/com/copay/app/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/copay/app/viewmodel/UserViewModel.kt
@@ -1,23 +1,27 @@
 package com.copay.app.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.copay.app.dto.response.LoginResponseDTO
+import com.copay.app.model.User
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 class UserViewModel : ViewModel() {
 
+    private val _user = MutableStateFlow<User?>(null)
+    val user: StateFlow<User?> = _user
 
-    private val _user = MutableStateFlow<LoginResponseDTO?>(null)
-    val user: StateFlow<LoginResponseDTO?> = _user
-
-
-    fun setUser(userResponse: LoginResponseDTO) {
-        _user.value = userResponse
+    // Update the UserViewModel with the extracted fields from the Backend response.
+    fun setUser(phoneNumber: String?, userId: Long?, username: String?, email: String?) {
+        Log.d("UserViewModel", "Setting user: phoneNumber=$phoneNumber, userId=$userId, username=$username, email=$email")
+        _user.value = User(
+            userId = userId,
+            username = username,
+            email = email,
+            phoneNumber = phoneNumber
+        )
+        Log.d("UserViewModel", "User set: ${_user.value}")
     }
-
 
     fun clearUser() {
         _user.value = null
