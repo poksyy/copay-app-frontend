6812cc8735dd6a327fb56a5c3b135a9d3286c0b9
Dynavy <dynavy@hotmail.com>
2025-04-06 21:46:37 +0200

    Refactor(UserViewModel): Delegate user data management to UserSession
    - Replaced direct user data management with UserSession for better separation of responsabilities.
    
    - UserSession now handles the persistence and retrieval of user data.
    
    - UserViewModel now only exposes the user state and provides a method to clear the user session.
    
    - Added Hilt dependency injection for UserSession.

--- app/src/main/java/com/copay/app/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/copay/app/viewmodel/UserViewModel.kt
@@ -1,29 +1,21 @@
 package com.copay.app.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.copay.app.model.User
-import kotlinx.coroutines.flow.MutableStateFlow
+import com.copay.app.utils.state.UserSession
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
 
-class UserViewModel : ViewModel() {
+@HiltViewModel
+class UserViewModel @Inject constructor(
+    private val userSession: UserSession
+) : ViewModel() {
 
-    private val _user = MutableStateFlow<User?>(null)
-    val user: StateFlow<User?> = _user
-
-    // Update the UserViewModel with the extracted fields from the Backend response.
-    fun setUser(phoneNumber: String?, userId: Long?, username: String?, email: String?) {
-        Log.d("UserViewModel", "Setting user: phoneNumber=$phoneNumber, userId=$userId, username=$username, email=$email")
-        _user.value = User(
-            userId = userId,
-            username = username,
-            email = email,
-            phoneNumber = phoneNumber
-        )
-        Log.d("UserViewModel", "User set: ${_user.value}")
-    }
+    val user: StateFlow<User?> = userSession.user
 
     fun clearUser() {
-        _user.value = null
+        userSession.clearUser()
     }
 }
+
