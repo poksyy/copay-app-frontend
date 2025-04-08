737b3a53c5baa47ba52c6f0acbe1177c10cea06c
Dynavy <dynavy@hotmail.com>
2025-04-08 20:30:38 +0200

    FEAT(DTO): New DTO response when fetching groups through backend.
    - Match the same fields than the backend DTO response.

---
+++ app/src/main/java/com/copay/app/dto/response/GetGroupsResponseDTO.kt
@@ -0,0 +1,10 @@
+package com.copay.app.dto.response
+import java.time.LocalDateTime
+
+data class GetGroupsResponseDTO(
+    val groupId: Long,
+    val groupName: String,
+    val createdBy: Long,
+    val createdAt: LocalDateTime,
+    val memberIds: List<Long>
+)
