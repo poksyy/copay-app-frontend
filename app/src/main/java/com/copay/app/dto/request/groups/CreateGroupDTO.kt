b35a8f627e2a37fa01ce9b94d04dd8b09dd399ca
Dynavy <dynavy@hotmail.com>
2025-04-16 20:11:20 +0200

    CHORE(CreateGroup): Changed variable names.

--- app/src/main/java/com/copay/app/dto/request/groups/CreateGroupDTO.kt
+++ app/src/main/java/com/copay/app/dto/request/groups/CreateGroupDTO.kt
@@ -1,11 +1,15 @@
 package com.copay.app.dto.request.groups
 
+/**
+ * Data class representing the request body for group creation.
+ */
 data class CreateGroupDTO(
 
-    val createdBy: Long,
+    val userId: Long,
     val groupName: String,
-    val groupDescription: String?,
+    val groupDescription: String,
     val estimatedPrice: Float,
     val currency: String,
-    val invitedMembers: List<String>
+    val invitedExternalMembers : List<String>,
+    val invitedCopayMembers: List<String>
 )
