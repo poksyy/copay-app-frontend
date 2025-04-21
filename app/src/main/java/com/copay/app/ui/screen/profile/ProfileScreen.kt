4014dd25207391391a76f97bfdf4b11e8bc32360
Pau Carrera <153611745+poksyy@users.noreply.github.com>
2025-04-21 22:41:12 +0200

    REFACTOR(ProfileScreen): Updated ProfileScreen layout with LazyColumn, improve button styles, add icons and new share button
    - Replaced static option list with LazyColumn for dynamic rendering of profile options.
    
    - Moved username Text composable higher in the layout for better visibility.
    
    - Replaced profile picture with a generic placeholder image.
    
    - Applied custom styling to the 'Edit Profile' button using OutlinedButton and RoundedCornerShape.
    
    - Added a new 'Share Profile' button with similar styling to the 'Edit Profile' button.
    
    - Integrated icons for profile options using Icon composables, mapping them to the corresponding option titles.

--- app/src/main/java/com/copay/app/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/copay/app/ui/screen/profile/ProfileScreen.kt
@@ -1,19 +1,24 @@
 package com.copay.app.ui.screen.profile
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -22,18 +27,24 @@ import com.copay.app.navigation.SpaScreens
 import com.copay.app.ui.components.LogoutButton
 import com.copay.app.viewmodel.AuthViewModel
 import com.copay.app.viewmodel.NavigationViewModel
+import com.copay.app.viewmodel.UserViewModel
 
 @Composable
 fun ProfileScreen(
-    navigationViewModel: NavigationViewModel = viewModel()
+    navigationViewModel: NavigationViewModel = viewModel(),
+    userViewModel: UserViewModel = hiltViewModel()
 ) {
     val authViewModel: AuthViewModel = hiltViewModel()
     var showLogoutDialog by remember { mutableStateOf(false) }
     val context = LocalContext.current
 
+    val user by userViewModel.user.collectAsState()
+    val username = user?.username ?: "Username"
+
     ProfileContent(
         onEditProfileClick = { navigationViewModel.navigateTo(SpaScreens.EditProfile) },
-        onLogoutClick = { showLogoutDialog = true }
+        onLogoutClick = { showLogoutDialog = true },
+        username = username
     )
 
     if (showLogoutDialog) {
@@ -47,56 +58,9 @@ fun ProfileScreen(
 @Composable
 private fun ProfileContent(
     onEditProfileClick: () -> Unit,
-    onLogoutClick: () -> Unit
+    onLogoutClick: () -> Unit,
+    username: String
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Image(
-            painter = painterResource(id = R.drawable.google_256),
-            contentDescription = "Profile Picture",
-            modifier = Modifier
-                .size(100.dp)
-                .clip(CircleShape)
-        )
-
-        TextButton(
-            onClick = onEditProfileClick,
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Text(
-                text = "Edit Profile",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.primary
-            )
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Text(
-            text = "Dylan Navarro",
-            style = MaterialTheme.typography.titleLarge.copy(color = MaterialTheme.colorScheme.onBackground)
-        )
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        ProfileOptionsList()
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        LogoutButton(text = "Log out", onClick = onLogoutClick)
-
-        Spacer(modifier = Modifier.height(24.dp))
-    }
-}
-
-@Composable
-private fun ProfileOptionsList() {
     val options = listOf(
         "Notifications",
         "Payment Methods",
@@ -108,14 +72,124 @@ private fun ProfileOptionsList() {
         "Account Settings"
     )
 
-    Column(modifier = Modifier.fillMaxWidth()) {
-        options.forEach { option ->
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(2.dp),
+        contentPadding = PaddingValues(bottom = 32.dp)
+    ) {
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.profile_picture),
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = username,
+                style = MaterialTheme.typography.titleLarge.copy(color = MaterialTheme.colorScheme.onBackground)
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                OutlinedButton(
+                    onClick = onEditProfileClick,
+                    border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground),
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier.weight(1f),
+                    contentPadding = PaddingValues(horizontal = 10.dp, vertical = 12.dp)
+                ) {
+                    Text(
+                        text = "Edit Profile",
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+                OutlinedButton(
+                    onClick = { /* TODO: Add share profile action to add as a friend (url) */ },
+                    border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground),
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier.weight(1f),
+                    contentPadding = PaddingValues(horizontal = 10.dp, vertical = 12.dp)
+                ) {
+                    Text(
+                        text = "Share Profile",
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+
+        items(options) { option ->
+            ProfileOptionItem(option = option)
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(32.dp))
+            LogoutButton(text = "Log out", onClick = onLogoutClick)
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+@Composable
+fun ProfileOptionItem(option: String) {
+    val optionIcons = mapOf(
+        "Notifications" to R.drawable.ic_notifications,
+        "Payment Methods" to R.drawable.ic_payment,
+        "Help & Support" to R.drawable.ic_help,
+        "Language Settings" to R.drawable.ic_language,
+        "Security" to R.drawable.ic_security,
+        "Privacy Policy" to R.drawable.ic_privacy,
+        "Terms of Service" to R.drawable.ic_terms,
+        "Account Settings" to R.drawable.ic_account
+    )
+
+    val iconResId = optionIcons[option] ?: R.drawable.ic_general // Default icon if none found
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 2.dp)
+            .clickable { /* TODO: Add business logic to options */ },
+        shape = RoundedCornerShape(12.dp),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = iconResId),
+                contentDescription = option,
+                modifier = Modifier.size(24.dp),
+                tint = MaterialTheme.colorScheme.onSurface
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
             Text(
                 text = option,
                 style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.weight(1f),
                 color = MaterialTheme.colorScheme.onSurface
             )
-            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
