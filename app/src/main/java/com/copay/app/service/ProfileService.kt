d489af887d0e2ec4a22e22dfc5f3e0dfa18b05db
Cristian Oraña <krischxn@gmail.com>
2025-04-22 16:58:40 +0200

    FEAT(API): Implement API call to update password.

--- app/src/main/java/com/copay/app/service/ProfileService.kt
+++ app/src/main/java/com/copay/app/service/ProfileService.kt
@@ -1,10 +1,13 @@
 package com.copay.app.service
 
+import android.util.Log
 import com.copay.app.config.ApiService
 import com.copay.app.dto.request.profile.UpdateEmailDTO
+import com.copay.app.dto.request.profile.UpdatePasswordDTO
 import com.copay.app.dto.request.profile.UpdatePhoneNumberDTO
 import com.copay.app.dto.request.profile.UpdateUsernameDTO
 import com.copay.app.dto.response.profile.EmailResponseDTO
+import com.copay.app.dto.response.profile.PasswordResponseDTO
 import com.copay.app.dto.response.profile.PhoneNumberResponseDTO
 import com.copay.app.dto.response.profile.UsernameResponseDTO
 import retrofit2.Response
@@ -21,5 +24,10 @@ class ProfileService(private val api: ApiService) {
 
     suspend fun updateEmail(request: UpdateEmailDTO): Response<EmailResponseDTO> {
         return api.updateEmail(request.userId, request)
+    }
+
+    suspend fun updatePassword(request: UpdatePasswordDTO, token: String): Response<PasswordResponseDTO> {
+        Log.d("DataStoreManager","update password SENDING TOKEN TO BACKEND $token")
+        return api.updatePassword(request, token)
     }
 }
