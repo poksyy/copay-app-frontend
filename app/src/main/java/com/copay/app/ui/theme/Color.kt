83b0cc1fb8bfa7d71305e502172262f9f8a41952
Cristian Oraña <krischxn@gmail.com>
2025-04-22 02:30:05 +0200

    FEAT(DarkMode): Create theme for each mode

--- app/src/main/java/com/copay/app/ui/theme/Color.kt
+++ app/src/main/java/com/copay/app/ui/theme/Color.kt
@@ -2,22 +2,25 @@ package com.copay.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Primary and Secondary for theme.kt
-val Primary = Color(0xFF000000) // Black
-val Secondary = Color(0xFFFFFFFF) // White
+// General
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
 
-// Main Colors
-val Black = Color(0xFF000000) // Black
-val White = Color(0xFFFFFFFF) // White
+// Light mode
+val LightBackground = Color(0xFFFFFFFF)
+val LightBackgroundText = Color(0xFF000000)
+val LightSecondaryText = Color(0xFF444444)
+val LightBorders = Color(0xFFB0B0B0)
+val LightAccent = Color(0xFF888888)
+
+// Dark mode
+val DarkBackground = Color(0xFF121212)
+val DarkBackgroundText = Color(0xFFE0E0E0)
+val DarkSecondaryText = Color(0xFFB0B0B0)
+val DarkBorders = Color(0xFF444444)
+val DarkAccent = Color(0xFF888888)
 
 // Button Colors
-// Main Button Colors
-val MainButtonBackground = Color(0xFF000000) // Black button
-
-// Secondary Button Colors
-val SecondaryButtonFill = Color(0xFFFFFFFF) // White button
-val SecondaryButtonBorder = Color(0xFF000000) // Black border
-
 // Logout Button
 val LogoutButtonBackground = Color(0xFFFF0000) // Red button
 
