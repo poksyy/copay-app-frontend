fa99c04906efee9e5524f7277de45582e87d615c
Cristian Oraña <oranacj@gmail.com>
2025-05-04 20:24:00 +0200

    FIX(GroupSession): Use groupDTO to simplify code.

--- app/src/main/java/com/copay/app/utils/session/GroupSession.kt
+++ app/src/main/java/com/copay/app/utils/session/GroupSession.kt
@@ -1,7 +1,5 @@
 package com.copay.app.utils.session
 
-import com.copay.app.dto.group.auxiliary.ExternalMemberDTO
-import com.copay.app.dto.group.auxiliary.RegisteredMemberDTO
 import com.copay.app.model.Group
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -13,24 +11,8 @@ class GroupSession @Inject constructor() {
     private val _group = MutableStateFlow<Group?>(null)
     val group: StateFlow<Group?> get() = _group
 
-    fun setGroup(
-        groupId: Long?,
-        groupName: String?,
-        description: String?,
-        currency: String?,
-        registeredMembers: List<RegisteredMemberDTO>?,
-        externalMembers: List<ExternalMemberDTO>?
-    ) {
-        if (groupId != null && groupName != null) {
-            _group.value = Group(
-                groupId = groupId,
-                name = groupName,
-                description = description,
-                currency = currency,
-                registeredMembers = registeredMembers,
-                externalMembers = externalMembers
-            )
-        }
+    fun setGroup(group: Group) {
+        _group.value = group
     }
 
     fun clearGroup() {
