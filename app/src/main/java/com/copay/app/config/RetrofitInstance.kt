97785bf6ebf4d52345fa4654f67dc2049717ce69
Pau Carrera <153611745+poksyy@users.noreply.github.com>
2025-05-13 17:19:18 +0200

    FEAT(Port): Added Noemi's phone port to test

--- app/src/main/java/com/copay/app/config/RetrofitInstance.kt
+++ app/src/main/java/com/copay/app/config/RetrofitInstance.kt
@@ -10,7 +10,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
 
     // Base URL for API calls.
-    private const val BASE_URL = "http://10.0.2.2:8080"
+    //private const val BASE_URL = "http://10.0.2.2:8080"
+    // Noemi's phone
+    private const val BASE_URL = "http://10.118.3.34:8080"
+
 
     // Create an OkHttpClient with logging interceptor for debugging.
     private val client by lazy {
