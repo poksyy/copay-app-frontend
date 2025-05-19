ecf352b23185d8a5440360dc0198ebdc767a4801
Pau Carrera <153611745+poksyy@users.noreply.github.com>
2025-05-19 14:02:10 +0200

    REFACTOR(EditPhoneNumberScreen): Added new top navigation bar, added spacing and button layout

--- app/src/main/java/com/copay/app/ui/screen/profile/edit/EditPhoneNumberScreen.kt
+++ app/src/main/java/com/copay/app/ui/screen/profile/edit/EditPhoneNumberScreen.kt
@@ -15,8 +15,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.copay.app.navigation.SpaScreens
 import com.copay.app.ui.components.dialog.logoutAfterPhoneChangeDialog
 import com.copay.app.ui.components.button.backButtonTop
+import com.copay.app.ui.components.button.secondaryButton
 import com.copay.app.ui.components.input.countriesList
 import com.copay.app.ui.components.input.inputField
+import com.copay.app.ui.components.topNavBar
 import com.copay.app.ui.theme.CopayColors
 import com.copay.app.ui.theme.CopayTypography
 import com.copay.app.utils.state.ProfileState
@@ -30,9 +32,9 @@ import com.copay.app.viewmodel.UserViewModel
 fun editPhoneNumberScreen(
     navigationViewModel: NavigationViewModel = viewModel(),
     userViewModel: UserViewModel = hiltViewModel(),
-    profileViewModel: ProfileViewModel = hiltViewModel()
+    profileViewModel: ProfileViewModel = hiltViewModel(),
+    authViewModel: AuthViewModel = hiltViewModel()
 ) {
-    val authViewModel: AuthViewModel = hiltViewModel()
     val context = LocalContext.current
     val user by userViewModel.user.collectAsState()
     val profileState by profileViewModel.profileState.collectAsState()
@@ -44,7 +46,6 @@ fun editPhoneNumberScreen(
     // Local states for managing the phone number value and errors
     var apiErrorMessage by remember { mutableStateOf<String?>(null) }
     var phoneNumberError by remember { mutableStateOf<String?>(null) }
-
     var phoneNumber by remember { mutableStateOf(user?.phoneNumber ?: "") }
     val selectedCountry by remember { mutableStateOf(country) }
 
@@ -53,12 +54,10 @@ fun editPhoneNumberScreen(
             is ProfileState.Success.PhoneUpdated -> {
                 authViewModel.logout(context)
             }
-
             is ProfileState.Error -> {
                 apiErrorMessage = (profileState as ProfileState.Error).message
                 profileViewModel.resetProfileState()
             }
-
             else -> {}
         }
     }
@@ -70,45 +69,20 @@ fun editPhoneNumberScreen(
     }
 
     Box(modifier = Modifier.fillMaxSize()) {
-        // Back button.
-        backButtonTop(
-            onBackClick = { navigationViewModel.navigateTo(SpaScreens.ProfileSubscreen.EditProfile )},
+        topNavBar(
+            title = "Edit phone number",
+            onBackClick = { navigationViewModel.navigateTo(SpaScreens.ProfileSubscreen.EditProfile) },
             modifier = Modifier
-                .padding(16.dp)
-                .align(Alignment.TopStart)
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
         )
 
-        TextButton(
-            onClick = {
-                validateInputs()
-                if (phoneNumberError == null) {
-                    showLogoutDialog = true
-                }
-            }, modifier = Modifier
-                .align(Alignment.TopEnd)
-                .padding(16.dp)
-        ) {
-            Text(
-                "Done",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.primary
-            )
-        }
-
-        // Screen content.
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(start = 16.dp, end = 16.dp, top = 72.dp),
-            verticalArrangement = Arrangement.spacedBy(24.dp)
+                .padding(horizontal = 24.dp)
+                .padding(top = 90.dp),
         ) {
-            Text(
-                "Edit Phone Number",
-                color = CopayColors.primary,
-                style = CopayTypography.title
-            )
-
-            // Text field for phone number
             inputField(
                 value = phoneNumber,
                 onValueChange = {
@@ -118,7 +92,7 @@ fun editPhoneNumberScreen(
                 },
                 label = "Phone number",
                 keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Email
+                    keyboardType = KeyboardType.Phone
                 ),
                 modifier = Modifier.fillMaxWidth(),
                 isError = phoneNumberError != null
@@ -135,12 +109,25 @@ fun editPhoneNumberScreen(
 
             // Phone number's description
             Text(
-                "Your phone number is used for account verification, recovery, " +
-                        "and notifications. Make sure it's a valid and active number " +
-                        "that you can be reached at.",
+                text = "Your phone number is used for account verification, recovery, and notifications. Make sure it's a valid and active number that you can be reached at.",
                 style = CopayTypography.footer,
                 color = CopayColors.surface,
                 modifier = Modifier.padding(top = 8.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            secondaryButton(
+                text = "Save changes",
+                onClick = {
+                    validateInputs()
+                    if (phoneNumberError == null) {
+                        showLogoutDialog = true
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
             )
         }
 
@@ -149,6 +136,7 @@ fun editPhoneNumberScreen(
                 onDismiss = { showLogoutDialog = false },
                 onConfirm = {
                     profileViewModel.updatePhoneNumber(context, phoneNumber)
+                    showLogoutDialog = false
                 }
             )
         }
