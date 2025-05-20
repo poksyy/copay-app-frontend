9e6bcdd96cca33567254785c26cd1ac2b653363b
Cristian Oraña <krischxn@gmail.com>
2025-05-20 23:10:34 +0200

    FIX(DTO): Message from backend was not saved

--- app/src/main/java/com/copay/app/dto/user/UserResponseDTO.kt
+++ app/src/main/java/com/copay/app/dto/user/UserResponseDTO.kt
@@ -4,5 +4,8 @@ data class UserResponseDTO(
     val id: Long,
     val username: String,
     val email: String,
-    val phoneNumber: String
+    val phoneNumber: String,
+
+    // Message in case of any error in the backend.
+    val message: String? = null
 )
