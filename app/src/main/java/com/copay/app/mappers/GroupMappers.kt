075f84072b42ab7be6fa59ff01b8b67dce7676b0
Cristian Oraña <krischxn@gmail.com>
2025-05-21 19:18:39 +0200

    REFACTOR(GroupMapper): Move file to /mappers

---
+++ app/src/main/java/com/copay/app/mappers/GroupMappers.kt
@@ -0,0 +1,26 @@
+package com.copay.app.mappers
+
+import com.copay.app.dto.group.response.CreateGroupResponseDTO
+import com.copay.app.model.Group
+
+/**
+ * Maps a GroupResponseDTO to a domain Group model
+ */
+fun CreateGroupResponseDTO.toGroup(): Group {
+    return Group(
+        groupId = this.groupId,
+        name = this.name,
+        description = this.description,
+        estimatedPrice = this.estimatedPrice,
+        currency = this.currency,
+        createdAt = this.createdAt,
+        isOwner = this.userIsOwner,
+        ownerId = this.groupOwner.ownerId,
+        ownerName = this.groupOwner.ownerName,
+        registeredMembers = this.registeredMembers,
+        externalMembers = this.externalMembers,
+        expenses = emptyList(),
+        imageUrl = null,
+        imageProvider = null
+    )
+}
