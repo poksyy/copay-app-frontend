6952e61e31ecedd778615f59a46df47a8522eb4d
Cristian Oraña <krischxn@gmail.com>
2025-05-27 00:35:40 +0200

    fix(UserResponseDTO): phone prefix is missing

--- app/src/main/java/com/copay/app/dto/user/UserResponseDTO.kt
+++ app/src/main/java/com/copay/app/dto/user/UserResponseDTO.kt
@@ -4,6 +4,7 @@ data class UserResponseDTO(
     val id: Long,
     val username: String,
     val email: String,
+    val phonePrefix: String,
     val phoneNumber: String,
 
     // Message in case of any error in the backend.
