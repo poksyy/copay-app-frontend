8705a27e7880e73a98eedec5009d1cc0a6d225e8
CristianJAradoxa <krischxn@gmail.com>
2025-05-27 14:47:00 +0200

    feat(expenseState): Implement new states for each total

--- app/src/main/java/com/copay/app/utils/state/ExpenseState.kt
+++ app/src/main/java/com/copay/app/utils/state/ExpenseState.kt
@@ -1,6 +1,8 @@
 package com.copay.app.utils.state
 
 import com.copay.app.dto.expense.response.GetExpenseResponseDTO
+import com.copay.app.dto.expense.response.TotalDebtResponseDTO
+import com.copay.app.dto.expense.response.TotalSpentResponseDTO
 import com.copay.app.dto.expense.response.UserExpenseDTO
 
 sealed class ExpenseState {
@@ -11,6 +13,10 @@ sealed class ExpenseState {
         data class ExpensesFetched(val expenses: List<GetExpenseResponseDTO>) : Success()
 
         data class ExpenseMembersIds(val expense: List<UserExpenseDTO>) : Success()
+
+        data class TotalDebt(val data: TotalDebtResponseDTO) : Success()
+
+        data class TotalSpent(val data: TotalSpentResponseDTO) : Success()
     }
 
     data class Error(val message: String) : ExpenseState()
