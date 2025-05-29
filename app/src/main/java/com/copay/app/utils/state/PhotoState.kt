318171e344866a3bcde80a76af0f4c8ee9beabc4
Pau Carrera <153611745+poksyy@users.noreply.github.com>
2025-05-29 15:03:30 +0200

    feat(Photo): Add more states for new endpoints

--- app/src/main/java/com/copay/app/utils/state/PhotoState.kt
+++ app/src/main/java/com/copay/app/utils/state/PhotoState.kt
@@ -1,6 +1,6 @@
 package com.copay.app.utils.state
 
-import com.copay.app.dto.unsplash.UnsplashResponse
+import com.copay.app.dto.unsplash.response.UnsplashResponse
 
 /**
  * Different photo states for UI handling
@@ -13,8 +13,17 @@ sealed class PhotoState {
     // Loading state while searching photos
     data object Loading : PhotoState()
 
-    // Photos loaded successfully
-    data class Success(val data: UnsplashResponse) : PhotoState()
+    // Success states
+    sealed class Success : PhotoState() {
+        // Photos loaded successfully
+        data class PhotoList(val data: UnsplashResponse) : Success()
+
+        // Photo set successfully
+        data class PhotoSet(val message: String) : Success()
+
+        // Photo removed successfully
+        data class PhotoRemoved(val message: String) : Success()
+    }
 
     // An error occurred while searching photos
     data class Error(val message: String) : PhotoState()
