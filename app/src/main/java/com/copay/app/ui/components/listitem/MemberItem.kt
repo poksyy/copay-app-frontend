4ece4c7269073c142d6cc16700c7be529facfc0c
Pau Carrera <153611745+poksyy@users.noreply.github.com>
2025-05-29 21:15:49 +0200

    delete(Import): Delete unnecessary imports

--- app/src/main/java/com/copay/app/ui/components/listitem/MemberItem.kt
+++ app/src/main/java/com/copay/app/ui/components/listitem/MemberItem.kt
@@ -1,6 +1,5 @@
 package com.copay.app.ui.components.listitem
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -8,9 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.copay.app.R
 import com.copay.app.dto.group.auxiliary.ExternalMemberDTO
 import com.copay.app.dto.group.auxiliary.RegisteredMemberDTO
